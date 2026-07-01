184020c027ac0c4e3c7bfd42c50f8875df1e7a3f
Mardakhay Ravinov <alfredrockankeur@gmail.com>
2026-07-01 10:50:05 +0400

    Add filter route params

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -3,11 +3,27 @@ import { SearchPage } from '@pages/search'
 
 interface SearchRouteParams {
   q?: string
+  genre?: string
+  platform?: string
+  year?: string
+  metacritic?: string
+  rating?: string
+  ordering?: string
+}
+
+function readString(value: unknown) {
+  return typeof value === 'string' && value.length > 0 ? value : undefined
 }
 
 export const Route = createFileRoute('/search')({
   validateSearch: (search: Record<string, unknown>): SearchRouteParams => ({
-    q: typeof search.q === 'string' ? search.q : undefined,
+    q: readString(search.q),
+    genre: readString(search.genre),
+    platform: readString(search.platform),
+    year: readString(search.year),
+    metacritic: readString(search.metacritic),
+    rating: readString(search.rating),
+    ordering: readString(search.ordering),
   }),
   component: SearchPage,
 })
