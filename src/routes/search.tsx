e5be802c910cdbde4b416a5eefb74ae7d332208d
Mardakhay Ravinov <alfredrockankeur@gmail.com>
2026-07-01 10:42:49 +0400

    Type search route params

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -1,6 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { SearchPage } from '@pages/search'
 
+interface SearchRouteParams {
+  q?: string
+}
+
 export const Route = createFileRoute('/search')({
+  validateSearch: (search: Record<string, unknown>): SearchRouteParams => ({
+    q: typeof search.q === 'string' ? search.q : undefined,
+  }),
   component: SearchPage,
 })
