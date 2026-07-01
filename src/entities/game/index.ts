a9c0b457db90d6cac77065de13f2f471c94eacd0
Mardakhay Ravinov <alfredrockankeur@gmail.com>
2026-07-01 10:34:41 +0400

    Export game api layer

--- src/entities/game/index.ts
+++ src/entities/game/index.ts
@@ -1,2 +1,19 @@
+export { gameQueryKeys } from './api/queryKeys'
+export { gameService } from './api/gameService'
+export { useGameDetailsQuery, useGamesQuery } from './api/useGamesQuery'
 export { GameCard, GameCardSkeleton } from './ui/GameCard'
 export type { GameCardGame } from './ui/GameCard'
+export type {
+  GameListParams,
+  RawgDeveloper,
+  RawgGame,
+  RawgGameDetails,
+  RawgGamePlatform,
+  RawgGenre,
+  RawgPaginatedResponse,
+  RawgPlatform,
+  RawgPlatformParent,
+  RawgPublisher,
+  RawgShortScreenshot,
+  RawgStore,
+} from './model/types'
